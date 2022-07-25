da51156690d2cb2a129860278baf5a8f4edc8c96
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-07-25 23:33:20 +0900

    상속

--- sample3.kt
+++ sample3.kt
@@ -1,6 +1,6 @@
 package com.example.practice1.junga.kotlinpractice
 
-class Human (val name : String = "unknown"){
+open class Human (val name : String = "unknown"){
 
     constructor(name : String, age : Int) : this(name){
         //부생성자는 주생성자에게 위임을 받아야한다 : this(name)
@@ -15,14 +15,25 @@ class Human (val name : String = "unknown"){
     fun eatingFood() {
         println("Yummy")
     }
+
+    fun singASong(){
+        println("오늘 달이 좋구나~~")
+    }
+}
+
+
+class Korea : Human() { //상속 부모 클래스에 open 예약어 필요
+
 }
 
 fun main(){
+    val Kpop = Korea()
+    Kpop.singASong()
 //    val human = Human("Xion")
 //    val stranger = Human()
 //    human.eatingFood()
 
-    val someone = Human("minji", 32)
+    //val someone = Human("minji", 32)
     //init 먼저 실행 그 다음 constructor 실행
 //    println("this human's name is ${human.name}")
 }
