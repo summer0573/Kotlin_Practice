4e864baa9fe9efa5c4b0aadfadfef8bcafa5847e
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-07-25 23:28:40 +0900

    부생성자

--- sample3.kt
+++ sample3.kt
@@ -2,6 +2,12 @@ package com.example.practice1.junga.kotlinpractice
 
 class Human (val name : String = "unknown"){
 
+    constructor(name : String, age : Int) : this(name){
+        //부생성자는 주생성자에게 위임을 받아야한다 : this(name)
+        println("my name is ${name}, ${age}years old")
+
+    }
+
     init { //주생성자
         println("New human had been born!")
     }
@@ -12,9 +18,11 @@ class Human (val name : String = "unknown"){
 }
 
 fun main(){
-    val human = Human("Xion")
+//    val human = Human("Xion")
+//    val stranger = Human()
+//    human.eatingFood()
 
-    //val stranger = Human()
-    human.eatingFood()
-    println("this human's name is ${human.name}")
+    val someone = Human("minji", 32)
+    //init 먼저 실행 그 다음 constructor 실행
+//    println("this human's name is ${human.name}")
 }
